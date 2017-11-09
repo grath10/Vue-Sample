4e09b9e78380231f1e2489d25764215a6359fd35
user <grath90@163.com>
2017-11-09 13:16:52 +0800

    pick up word from pic captcha

--- src/main/java/com/demo/config/WebSecurityConfig.java
+++ src/main/java/com/demo/config/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.demo.config;
 
-import com.demo.service.CustomAuthenticationProvider;
+import com.demo.service.TextAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private CustomAuthenticationProvider authenticationProvider;
+    private TextAuthenticationProvider authenticationProvider;
     @Autowired
     private AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/login", "/gt/register").permitAll()
+                .antMatchers("/login", "/gt/register", "/imagecode").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
